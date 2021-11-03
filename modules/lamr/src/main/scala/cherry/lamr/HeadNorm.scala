00530b7bce89f8a9be13fa9cc8144e51a1ca0832
Oleg Nizhnik <odomontois@gmail.com>
2021-11-03 19:36:00 +0300

    +++

--- modules/lamr/src/main/scala/cherry/lamr/HeadNorm.scala
+++ modules/lamr/src/main/scala/cherry/lamr/HeadNorm.scala
@@ -5,16 +5,21 @@ import cats.data.EitherK
 import scala.annotation.unchecked.uncheckedVariance
 import java.text.Normalizer
 
+enum Res:
+  case Error(message: String, other: Vector[String] = Vector.empty)
+  case Stop
+  case Ok(res: NormVal)
+
 trait Custom[+R]:
   def maybeNorm: Fix[Lang]
-  def apply(context: NormVal, arg: NormVal): Either[String, NormVal]
-  def andThen(pre: NormVal): Either[String, NormVal]
+  def apply(context: NormVal, arg: NormVal): Res
+  def andThen(pre: NormVal): Res
 
 type NormV[+A] = EitherK[Custom, Lang, A @uncheckedVariance]
 type NormVal   = Fix[NormV]
 
 trait NormPack:
-  def resolve(ref: LibRef): Either[String, NormVal]
+  def resolve(ref: LibRef): Res
 
 class Normalizer(packs: Map[String, NormPack]):
-  def headNorm(lang: Fix[Lang]): Either[String, NormVal] = ???
+  def headNorm(lang: Fix[Lang]): Res = ???
