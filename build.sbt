087e8ebb5b37592d06291c0b2f2f68165037f506
Oleg Nizhnik <o.nizhnikov@tinkoff.ru>
2022-01-18 16:04:40 +0300

    ---

--- build.sbt
+++ build.sbt
@@ -2,20 +2,28 @@ ThisBuild / scalaVersion := "3.1.0"
 
 val modules = file("modules")
 
+publish / skip := true
+
+val publishSettings = Vector(
+  organization := "ru.tinkoff",
+  version      := "0.0.1-Check4-SNAPSHOT",
+)
+
 val testDependencies = libraryDependencies ++= Vector(
   "org.scalactic" %% "scalactic" % Version.scalaTest % Test,
   "org.scalatest" %% "scalatest" % Version.scalaTest % Test,
 )
 
 val dependencies2 = Vector(
-  "tf.tofu"       %% "tofu-core-ce3" % Version.tofu,
-  "org.typelevel" %% "cats-free"     % Version.cats,
+  "tf.tofu"       %% "tofu-kernel" % Version.tofu,
+  "org.typelevel" %% "cats-free"   % Version.cats,
 ).map(_.cross(CrossVersion.for3Use2_13))
 
-val coreDependencies = libraryDependencies ++= Vector(
-) ++ dependencies2
+val coreDependencies = libraryDependencies ++= dependencies2
 
-lazy val lamr        = project
+lazy val lamr = project
   .in(modules / "lamr")
+  .settings(name := "cherry-lamr")
+  .settings(publishSettings)
   .settings(coreDependencies)
   .settings(testDependencies)
--- modules/lamr/src/main/scala/cherry/fix/Fix.scala
+++ modules/lamr/src/main/scala/cherry/fix/Fix.scala
@@ -6,26 +6,27 @@ import cats.Traverse
 import cats.Eval
 import tofu.syntax.collections.given
 
-type Fix[+G[+_]] = Fix.T[G]
+type Fix[+G[+_]] = Fix.Fix[G]
 
 object Fix:
-  opaque type T[+G[+_]] >: G[Nothing] <: G[Any] = G[Any]
+  opaque type Fix[+G[+_]] >: G[Nothing] <: G[Any] = G[Any]
 
-  def apply[G[+_]](gf: G[T[G]]): T[G] = gf
+  def apply[G[+_]](gf: G[Fix[G]]): Fix[G] = gf
 
-  extension [G[+_]](tg: T[G])
-    def unpack: G[T[G]] = tg.asInstanceOf[G[T[G]]]
+  extension [G[+_]](fix: Fix[G])
+    def unpack: G[Fix[G]] = fix.asInstanceOf[G[Fix[G]]]
 
     def fold[R](f: G[R] => R)(using Functor[G]): R =
-      f(tg.unpack.map(_.fold(f)))
+      f(fix.unpack.map(_.fold(f)))
 
     def foldDefer[R](f: G[Eval[R]] => Eval[R])(using Traverse[G]): Eval[R] =
-      Eval.defer(f(tg.unpack.map(_.foldDefer(f))))
+      Eval.defer(f(fix.unpack.map(_.foldDefer(f))))
 
     def folds[R](f: G[R] => R)(using Traverse[G]): R =
-      tg.foldDefer[R](_.sequence.map(f)).value
+      fix.foldDefer[R](_.sequence.map(f)).value
 
-    def foldHist[R](f: G[Cofree[G, R]] => Eval[R])(using Traverse[G]): Cofree[G, R] = {
-      val gc = Eval.later(tg.unpack.map(_.foldHist(f)))
+    def foldHist[R](f: G[Cofree[G, R]] => Eval[R])(using Traverse[G]): Cofree[G, R] =
+      val gc = Eval.later(fix.unpack.map(_.foldHist(f)))
       Cofree(gc.flatMap(f).memoize, gc)
-    }
+    end foldHist
+end Fix
