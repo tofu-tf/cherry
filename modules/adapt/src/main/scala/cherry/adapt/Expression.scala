fc26f7ed7504c2914921de8b10141e60a964ad92
Odomontois <odomontois@gmail.com>
2022-02-19 00:00:47 +0300

    adapt parse (#3)

--- modules/adapt/src/main/scala/cherry/adapt/Expression.scala
+++ modules/adapt/src/main/scala/cherry/adapt/Expression.scala
@@ -1,10 +1,17 @@
 package cherry.adapt
 
+import cats.parse.Parser
 import cherry.fix.Fix.Fix
 import cherry.lamr.Lang
 import cherry.lamr.norm.{Library, NormValue, Normalizer, Term}
+import cherry.lamr.parse
 
-class Expression(expr: Term):
+class Expression(val expr: Term):
   def unpack: Lang[Expression] =
     expr.unpack match
       case l: Lang[Term] => l.map(Expression(_))
+
+object Parsing:
+  val sourceTerm: Parser[Term] = parse.term.source
+
+  val sourceExpression: Parser[Expression] = sourceTerm.map(Expression(_))
