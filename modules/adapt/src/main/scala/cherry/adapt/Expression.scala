a73089ab7ec2f1cb310e4180f178bf44a1b1fb94
Oleg Nizhnik <odomontois@gmail.com>
2022-02-22 11:16:08 +0300

    Add adapted normalize

--- modules/adapt/src/main/scala/cherry/adapt/Expression.scala
+++ modules/adapt/src/main/scala/cherry/adapt/Expression.scala
@@ -1,15 +1,19 @@
 package cherry.adapt
 
+import cats.data.NonEmptyVector
 import cats.parse.Parser
 import cherry.fix.Fix.Fix
-import cherry.lamr.Lang
-import cherry.lamr.norm.{Library, NormValue, Normalizer, Term}
-import cherry.lamr.parse
+import cherry.lamr.{Lang, norm, parse}
+import cherry.lamr.norm.{BuiltinLibrary, Library, NormValue, Normalizer, State, Term}
 
 class Expression(val expr: Term):
   def unpack: Lang[Expression] =
     expr.unpack match
       case l: Lang[Term] => l.map(Expression(_))
+
+  def normalize(n: Normalizer, context: NormValue = BuiltinLibrary): Either[Vector[norm.Error], NormValue] =
+    val state = State()
+    n.normalize(expr, context).run(state).toRight(state.errors)
 
 object Parsing:
   val sourceTerm: Parser[Term] = parse.term.source
