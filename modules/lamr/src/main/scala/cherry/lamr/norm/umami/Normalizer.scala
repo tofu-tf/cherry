a51189648fae1af87f4920ecbb00f7f6e20fbc36
Oleg Nizhnik <odomontois@gmail.com>
2022-08-26 09:12:46 +0000

    aaaa restore my normalizer already

--- modules/lamr/src/main/scala/cherry/lamr/norm/umami/Normalizer.scala
+++ modules/lamr/src/main/scala/cherry/lamr/norm/umami/Normalizer.scala
@@ -1,64 +1,60 @@
 package cherry.lamr.norm
 package umami
 
-import cats.syntax.parallel.given
 import cherry.lamr.{Lang, LibRef, TypeOptions}
-import cherry.utils.Act
-import tofu.syntax.monadic.given
+import cherry.utils.{Act, ActMethods, Raising}
+
 class UmamiNormalizer(library: Library, dbg: (Term, cherry.lamr.norm.NormValue, State) => Unit = (_, _, _) => ())
     extends Normalizer:
 
-  def normalize(term: Term, context: NormValue): Process[NormValue] =
-    Act.action(st => dbg(term, context, st)) >> bigStep(term, context)
+  def normalize(term: Term): Process[NormValue]          =
+    Process.context.flatMap(context => Process.action(ctx => dbg(term, context, ctx.state))) >> bigStep(term)
 
-  private def bigTypeStep(term: Term, context: NormValue): Process[NormType] =
-    normalize(term, context).flatMap(_.asType)
+  private def bigTypeStep(term: Term): Process[NormType] =
+    normalize(term).flatMap(_.asType)
 
-  private def bigStep(term: Term, context: NormValue): Process[NormValue] = term.unpack match
+  // normalization catamorphism
+  private def bigStepOnce(lang: Lang[Process[NormValue]]): Process[NormValue] = lang match
+    case Lang.External(ref)  => library.resolve(ref, this)
+    case Lang.Universe(opts) => Process.pure(UniverseType(opts))
+    case Lang.Builtin(bt)    => Process.pure(BuiltinNormType(bt))
 
-    case Lang.External(ref) => library.resolve(context, ref, this)
-
-    case Lang.Universe(opts) => Act.pure(UniverseType(opts))
-
-    case Lang.Builtin(bt) => Act.pure(BuiltinNormType(bt))
-
-    case Lang.Extend(tb, te) =>
+    case Lang.Extend(baseProc, extProc) =>
       for
-        baseNorm <- normalize(tb, context)
+        baseNorm <- baseProc
         baseType <- baseNorm.asType
         absBase  <- baseType.asAbstract
-        extCtx   <- context.merge(absBase)
-        extNorm  <- normalize(te, extCtx)
+        extNorm  <- extProc.locallyMergeContext(absBase)
         extType  <- extNorm.asType
       yield ExtendType(baseType, extType)
 
     case Lang.Function(domain, effect, body) =>
       for
-        domType  <- bigTypeStep(domain, context)
-        domTerm  <- domType.asAbstract
-        extCtx   <- context.merge(domTerm)
-        bodyType <- bigTypeStep(body, extCtx)
-        effType  <- bigTypeStep(effect, context)
+        domType  <- domain >>= (_.asType)
+        domAbs   <- domType.asAbstract
+        bodyType <- (body >>= (_.asType)).locallyMergeContext(domAbs)
+        effType  <- effect >>= (_.asType)
       yield FunctionType(domType, effType, bodyType)
 
     case Lang.Merge(base, ext) =>
       for
-        baseNorm <- normalize(base, context)
-        extCtx   <- context.merge(baseNorm)
-        extNorm  <- normalize(ext, extCtx)
+        baseNorm <- base
+        extNorm  <- ext.locallyMergeContext(baseNorm)
         result   <- baseNorm.merge(extNorm)
       yield result
 
     case Lang.Capture(domain, body) =>
-      for domType <- bigTypeStep(domain, context)
-      yield Closure(context, body, domType, this)
+      for
+        domType <- domain >>= (_.asType)
+        context <- Process.context
+      yield Closure(context, body, domType)
 
     case Lang.Apply =>
-      context.first.flatMap2Par(context.second)(_.apply(_))
+      Process.context.flatMap(context => context.first.flatMap2Par(context.second)(_.apply(_)))
 
     case Lang.Record(k, t, _) =>
       for
-        tnorm <- normalize(t, context)
+        tnorm <- t
         ttype <- tnorm.asType
       yield RecordType.single(k, ttype)
 
@@ -68,17 +64,87 @@ class UmamiNormalizer(library: Library, dbg: (Term, cherry.lamr.norm.NormValue, 
     case Lang.Float(f)   => Act.pure(FloatValue(f))
     case Lang.Bool(b)    => Act.pure(BooleanValue(b))
 
-    case Lang.Id => Act.pure(context)
+    case Lang.Id => Process.context
 
-    case Lang.Set(k, t) => for tnorm <- normalize(t, context) yield RecordValue.single(k, tnorm)
+    case Lang.Set(k, t) => for tnorm <- t yield RecordValue.single(k, tnorm)
 
-    case Lang.GetKey(k, up) => context.get(k, up)
+    case Lang.GetKey(k, up) => Process.context.flatMap(_.get(k, up))
 
     case Lang.AndThen(tl, tr) =>
       for
-        left  <- normalize(tl, context)
-        right <- normalize(tr, left)
+        left  <- tl
+        right <- tr.locally(_.context = left)
       yield right
 
     case Lang.Narrow(t, domain) =>
-      normalize(t, context).flatMap2Par(normalize(domain, context).flatMap(_.asType))(_.narrow(_))
+      t.flatMap2Par(domain >>= (_.asType))(_.narrow(_))
+
+  private def bigStep(term: Term): Process[NormValue]                         = term.foldDefer(bigStepOnce)
+
+  private def bigStepOld(term: Term): Process[NormValue] = term.unpack match
+
+    case Lang.External(ref) => library.resolve(ref, this)
+
+    case Lang.Universe(opts) => Act.pure(UniverseType(opts))
+
+    case Lang.Builtin(bt) => Act.pure(BuiltinNormType(bt))
+
+    case Lang.Extend(tb, te) =>
+      for
+        baseNorm <- normalize(tb)
+        baseType <- baseNorm.asType
+        absBase  <- baseType.asAbstract
+        extNorm  <- normalize(te).locallyMergeContext(absBase)
+        extType  <- extNorm.asType
+      yield ExtendType(baseType, extType)
+
+    case Lang.Function(domain, effect, body) =>
+      for
+        domType  <- bigTypeStep(domain)
+        domTerm  <- domType.asAbstract
+        bodyType <- bigTypeStep(body).locallyMergeContext(domTerm)
+        effType  <- bigTypeStep(effect)
+      yield FunctionType(domType, effType, bodyType)
+
+    case Lang.Merge(base, ext) =>
+      for
+        baseNorm <- normalize(base)
+        extNorm  <- normalize(ext).locallyMergeContext(baseNorm)
+        result   <- baseNorm.merge(extNorm)
+      yield result
+
+    case Lang.Capture(domain, body) =>
+      for
+        domType <- bigTypeStep(domain)
+        context <- Process.context
+      yield Closure(context, normalize(body), domType)
+
+    case Lang.Apply =>
+      Process.context.flatMap(context => context.first.flatMap2Par(context.second)(_.apply(_)))
+
+    case Lang.Record(k, t, _) =>
+      for
+        tnorm <- normalize(t)
+        ttype <- tnorm.asType
+      yield RecordType.single(k, ttype)
+
+    case Lang.Unit       => Act.pure(UnitValue)
+    case Lang.Integer(i) => Act.pure(IntegerValue(i))
+    case Lang.Str(s)     => Act.pure(StringValue(s))
+    case Lang.Float(f)   => Act.pure(FloatValue(f))
+    case Lang.Bool(b)    => Act.pure(BooleanValue(b))
+
+    case Lang.Id => Process.context
+
+    case Lang.Set(k, t) => for tnorm <- normalize(t) yield RecordValue.single(k, tnorm)
+
+    case Lang.GetKey(k, up) => Process.context.flatMap(_.get(k, up))
+
+    case Lang.AndThen(tl, tr) =>
+      for
+        left  <- normalize(tl)
+        right <- normalize(tr).locally(_.context = left)
+      yield right
+
+    case Lang.Narrow(t, domain) =>
+      normalize(t).flatMap2Par(normalize(domain).flatMap(_.asType))(_.narrow(_))
